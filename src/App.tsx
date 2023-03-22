5d964f996098d1b8e12d52149d72066142132cb0
Dreezy305 <bankoleidris@gmail.com>
2023-03-22 23:31:38 +0100

    added react query, data-grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/dashboard/dashboard";
 import Games from "./pages/games/games";
@@ -8,25 +9,28 @@ import Users from "./pages/users/users";
 import { ColorModeContext, useMode } from "./theme";
 
 function App() {
+  const queryClient = new QueryClient();
   const [theme, colorMode] = useMode();
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <SideBarProvider>
-          <div style={{ height: "100%", width: "100%" }}>
-            <main>
-              <Topbar />
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/users" element={<Users />} />
-                <Route path="/games" element={<Games />} />
-              </Routes>
-            </main>
-          </div>
-        </SideBarProvider>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+    <QueryClientProvider client={queryClient}>
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <SideBarProvider>
+            <div style={{ height: "100%", width: "100%" }}>
+              <main>
+                <Topbar />
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/users" element={<Users />} />
+                  <Route path="/games" element={<Games />} />
+                </Routes>
+              </main>
+            </div>
+          </SideBarProvider>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/users/users.tsx
+++ src/pages/users/users.tsx
@@ -3,7 +3,7 @@ import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettin
 import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
 import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
 import { Box, Typography, useTheme } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import React from "react";
 import Header from "../../components/Header";
 import { tokens } from "../../theme";
@@ -11,5 +11,54 @@ import { tokens } from "../../theme";
 export default function Users() {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
-  return <Box m={"20px"}>users</Box>;
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 90 },
+    { field: "avatar", headerName: "Image", width: 110 },
+    { field: "name", headerName: "Name", width: 200 },
+    { field: "email", headerName: "Email", width: 200 },
+    { field: "address", headerName: "Address", width: 120 },
+    { field: "phoneNumber", headerName: "Phone Number", width: 120 },
+  ];
+  return (
+    <Box m={"20px"}>
+      {/* Header */}
+      <Box
+        display={"flex"}
+        justifyContent="space-between"
+        alignItems={"center"}
+      >
+        <Header title="Users" subtitle="Table below displays users data" />
+      </Box>
+      {/* TABLE / DATA GRID */}
+      <Box
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+        }}
+        m="8px 0 0 0"
+        height="80vh"
+      ></Box>
+    </Box>
+  );
 }
