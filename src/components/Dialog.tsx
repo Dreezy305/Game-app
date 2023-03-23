437cc68a18eb3f5249be37c1387663926d8711f0
Dreezy305 <bankoleidris@gmail.com>
2023-03-23 08:46:59 +0100

    delete useer data, delete user dialog

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,12 +1,17 @@
 import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
 import { LoadingButton } from "@mui/lab";
+import { Box } from "@mui/material";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+import { useTheme } from "@mui/material/styles";
+import { TransitionProps } from "@mui/material/transitions";
 import React from "react";
+import { tokens } from "../theme";
 import { DialogProps } from "../utils/interfaces";
 
 function DialogBox({
@@ -17,32 +22,52 @@ function DialogBox({
   loading,
   handleDelete,
 }: DialogProps): JSX.Element {
+  // PICK COLORS
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  // DIALOG TRANSITION
+  const Transition = React.forwardRef(function Transition(
+    props: TransitionProps & {
+      children: React.ReactElement<any, any>;
+    },
+    ref: React.Ref<unknown>
+  ) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  });
   return (
     <Dialog
       open={open}
       onClose={handleClose}
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
+      TransitionComponent={Transition}
+      transitionDuration={1}
     >
-      <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
-      <DialogContent>
-        <DialogContentText id="alert-dialog-description">
-          {`would you like to delete ${userName} data`}
-        </DialogContentText>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose}>Cancel</Button>
-        <LoadingButton
-          size="small"
-          onClick={handleDelete}
-          endIcon={<DeleteOutlinedIcon />}
-          loading={loading}
-          loadingPosition="end"
-          variant="contained"
-        >
-          <span>Delete</span>
-        </LoadingButton>
-      </DialogActions>
+      <Box sx={{ backgroundColor: colors.blueAccent[700] }}>
+        <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {`would you like to delete ${userName} data`}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} sx={{ color: colors.grey[700] }}>
+            Cancel
+          </Button>
+          <LoadingButton
+            size="small"
+            onClick={handleDelete}
+            endIcon={<DeleteOutlinedIcon />}
+            loading={loading}
+            loadingPosition="end"
+            variant="contained"
+            sx={{ backgroundColor: colors.redAccent[700] }}
+          >
+            <span>Delete</span>
+          </LoadingButton>
+        </DialogActions>
+      </Box>
     </Dialog>
   );
 }
