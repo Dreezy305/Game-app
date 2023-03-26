632344c14d32755d1bfb67c7cb4e5e4a9ae1c794
Dreezy305 <bankoleidris@gmail.com>
2023-03-26 08:26:41 +0100

    delete game

--- src/pages/games/overview/singleGame.tsx
+++ src/pages/games/overview/singleGame.tsx
@@ -1,6 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import ArrowBackOutlinedIcon from "@mui/icons-material/ArrowBackOutlined";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ModeOutlinedIcon from "@mui/icons-material/ModeOutlined";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function SingleGame(): JSX.Element {
+  const navigation = useNavigate();
   return <div>singleGame</div>;
 }
 
