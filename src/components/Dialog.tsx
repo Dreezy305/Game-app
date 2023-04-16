81be7720da30380ad65147989c9aab1b8af68f0b
Dreezy305 <bankoleidris@gmail.com>
2023-04-16 10:43:40 +0100

    removed unneccesary div wrappers

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -37,47 +37,45 @@ function DialogBox({
     return <Slide direction="up" ref={ref} {...props} />;
   });
   return (
-    <div>
-      <BootstrapDialog
-        onClose={handleClose}
-        aria-labelledby="customized-dialog-title"
+    <BootstrapDialog
+      onClose={handleClose}
+      aria-labelledby="customized-dialog-title"
+      open={open}
+    >
+      <Dialog
         open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        TransitionComponent={Transition}
+        transitionDuration={1}
       >
-        <Dialog
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-          TransitionComponent={Transition}
-          transitionDuration={1}
-        >
-          <Box sx={{ backgroundColor: colors.blueAccent[700] }}>
-            <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
-            <DialogContent>
-              <DialogContentText id="alert-dialog-description">
-                {`would you like to delete ${userName} data`}
-              </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={handleClose} sx={{ color: colors.grey[700] }}>
-                Cancel
-              </Button>
-              <LoadingButton
-                size="small"
-                onClick={handleDelete}
-                endIcon={<DeleteOutlinedIcon />}
-                loading={loading}
-                loadingPosition="end"
-                variant="contained"
-                sx={{ backgroundColor: colors.redAccent[700] }}
-              >
-                <span>Delete</span>
-              </LoadingButton>
-            </DialogActions>
-          </Box>
-        </Dialog>
-      </BootstrapDialog>
-    </div>
+        <Box sx={{ backgroundColor: colors.blueAccent[700] }}>
+          <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              {`would you like to delete ${userName} data`}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} sx={{ color: colors.grey[700] }}>
+              Cancel
+            </Button>
+            <LoadingButton
+              size="small"
+              onClick={handleDelete}
+              endIcon={<DeleteOutlinedIcon />}
+              loading={loading}
+              loadingPosition="end"
+              variant="contained"
+              sx={{ backgroundColor: colors.redAccent[700] }}
+            >
+              <span>Delete</span>
+            </LoadingButton>
+          </DialogActions>
+        </Box>
+      </Dialog>
+    </BootstrapDialog>
   );
 }
 
